ec4b32a69e4cae966ec352ea095a31c8602bf57b
Kendall Comey <kendall.comey@gmail.com>
2015-06-11 12:43:36 -0700

    Getting to delete function

--- js/noteList.js
+++ js/noteList.js
@@ -1,10 +1,18 @@
 var React = require('react')
 
 var NoteList = React.createClass({
+  handleDelete: function(){
+    this.props.handleDelete()
+  },
   render: function(){
     var listNotes = this.props.notes.map(function(note){
-      return <li> {note.note} ({note.author}) </li>;
-    });
+      return (
+        <li>
+          {note.note} {note.author}
+          [<a href="#" onClick={this.props.handleDelete}>x</a>]
+        </li>
+      )
+    }.bind(this));
     return (
         <div>
           <h3> Notes </h3>
@@ -17,3 +25,4 @@ var NoteList = React.createClass({
 })
 
 module.exports = NoteList;
+
